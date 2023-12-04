1ed8d8d8e6fb6b6ae67214dc53909cbf80108e97
Mohan Subramanian <maraudermohan@gmail.com>
2023-12-04 14:19:13 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Tamil Charades',
-  description: 'Charades game for Tamil movies. Developed by Mohan Subramanian.',
+  description: 'Dumb Charades game for Tamil movies. Supports multiple modes like Classic, Story and Song modes. 1960s movies till latest. Developed by Mohan Subramanian.',
   icons: {
     icon: '/favicon.png',
     apple: '/favicon-180x180.png',
