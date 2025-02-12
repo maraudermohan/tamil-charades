884825ea4b753aba46566864453828c10a633b58
Mohan Subramanian <msubramanian@8232-laptop.corp.zooxlabs.com>
2025-02-11 16:15:23 -0800

    Deferred loading of homepage images

--- src/components/ModeCard.tsx
+++ src/components/ModeCard.tsx
@@ -1,4 +1,5 @@
-import { memo } from "react";
+"use client";
+import { memo, useEffect, useState } from "react";
 import styles from "./ModeCard.module.css";
 import { rubik } from "app/fonts";
 import { GAME_MODES_DATA } from "constant";
@@ -9,15 +10,32 @@ interface ModeCardType {
 }
 
 function ModeCard({ modeData }: ModeCardType) {
+  const [isFirstLoadDone, setIsFirstLoadDone] = useState(false);
+
+  useEffect(() => {
+    const deferredImg = document.createElement("img");
+    deferredImg.src = GAME_MODES_DATA[modeData].backgroundImage;
+    deferredImg.onload = () => {
+      setIsFirstLoadDone(true);
+    };
+
+    return () => {
+      deferredImg.remove();
+    }
+  }, []);
+
   return (
     <Link
       className={styles.modeCardBox}
-      style={{
-        backgroundImage: `url(${GAME_MODES_DATA[modeData].backgroundImage})`,
-        backgroundPosition: GAME_MODES_DATA[modeData].backgroundPosition,
-      }}
       href={`game/${modeData}`}
     >
+      {isFirstLoadDone && (
+        <img
+          className={styles.modeImage}
+          alt={GAME_MODES_DATA[modeData].title}
+          src={GAME_MODES_DATA[modeData].backgroundImage}
+        />
+      )}
       <div className={styles.overlay}>
         <h3
           className={styles.title}
